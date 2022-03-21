82c50a50fce002aadf8187b2d6ef5254e5f28164
Lucas16AR <luqui0008@gmail.com>
2022-03-21 18:58:27 -0300

    creacion de carpetas backend y frontend

---
+++ backend/install.sh
@@ -0,0 +1,4 @@
+python3 -m venv . #crear entorno virtual
+source bin/activate #activa entorno
+pip install --upgrade pip #actualizar pip si es posible
+pip3 install -r requirements.txt #instala librerias necesarias
